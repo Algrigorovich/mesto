c2e0ce302a18ead2d43e0f07f70f20628fe79176
Alexander Grigorovich <crit1k8912@gmail.com>
2022-03-23 20:52:06 +0300

    fix: remove unnessesary constants

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,7 +4,6 @@ export const profileInfo = '.profile__job';
 export const cardsList = '.gallery__list';
 export const cardSelector = '.gallery-item-template';
 // Попапы
-export const popups = document.querySelectorAll('.popup');
 export const profileEditPopup ='.popup_edit-profile';
 export const addCardPopup = '.popup_add-item';
 export const imgPopupSelector = '.popup_fullscreen-img';
@@ -12,12 +11,6 @@ export const imgPopupSelector = '.popup_fullscreen-img';
 // Элементы модалки
 export const popupProfileOpenButton = document.querySelector('.profile__edit-btn');
 export const popupAddOpenButton = document.querySelector('.profile__add-btn');
-export const popupImageLink = document.querySelector('.popup__img');
-export const popupImageName = document.querySelector('.popup__img-name');
-
-// Формы
-export const profileEditForm = document.querySelector('#profile-edit-form');
-export const addCardForm = document.querySelector('#add-cart-form');
 
 // Инпуты
 export const nameInput = document.querySelector('#name');
